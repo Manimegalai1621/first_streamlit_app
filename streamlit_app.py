0c7f1247887e8da9f56acbc64efc1019617445a7
Manimegalai <114663217+Manimegalai1621@users.noreply.github.com>
2022-09-29 17:35:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.title("My Parents new healthy dinner");
+st.title("My Parents new healthy dinner")
+
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale,Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
